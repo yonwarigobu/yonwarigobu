9290bc521fb42f7a05d97c1f2a8773079e38e7ba
KoutarouY <kou06yokohama@gmail.com>
2022-05-29 19:24:03 +0900

    router入れました

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from "vue"
 import App from "./App.vue"
 import "./firebase.js"
-createApp(App).mount("#app")
+import router from "./router"
+createApp(App).use(router).mount("#app")
