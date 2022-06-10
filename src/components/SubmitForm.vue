3221fbce65aa08dc7a17a3454d1c24b08d647b3a
KandaSoutaro <s-kanda-4z4@eagle.sophia.ac.jp>
2022-06-10 16:21:23 +0900

    投稿フォームを実装しようとしたのですが上手くいっていません。。ヘッダーも出来てないです...申し訳ありません..

---
+++ src/components/SubmitForm.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="form__wrapper">
+    <textarea
+      class="form__textarea"
+      v-model="text"
+      placeholder="いまどうしてる？"
+    />
+    <div class="form__buttons">
+      <button v-on:click="postTweet" class="form__submit-button">投稿</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      text: "",
+    }
+  },
+  methods: {
+    postTweet() {
+      alert("投稿機能の完成をお楽しみに！")
+    },
+  },
+}
+</script>
+
+<style scoped>
+.form__wrapper {
+  padding: 1rem;
+}
+.form__textarea {
+  width: 100%;
+  height: calc(1.3rem * 3 + 0.5rem * 2);
+  padding: 0.5rem;
+  line-height: 1.3rem;
+  border-radius: 5px;
+  border: none;
+  resize: none;
+}
+.form__textarea:focus {
+  outline: none;
+}
+.form__buttons {
+  display: flex;
+  justify-content: flex-end;
+}
+</style>
--- src/views/PostApp.vue
+++ src/views/PostApp.vue
@@ -1,5 +1,84 @@
 <template>
   <div class="post">
-    <h1>This is post</h1>
+    <h1>投稿ページ</h1>
+  </div>
+
+  <div class="home__wrapper">
+    <submit-form />
+  </div>
+
+  <div class="app">
+    <button v-on:click="postTweet">投稿</button>
+    <!-- 変更点１ -->
+    <div>
+      <p v-for="tweet in tweets" :key="tweet.id">
+        {{ tweet.text }}
+        {{ tweet.place }}
+        {{ tweet.people }}
+        {{ tweet.money }}
+      </p>
+    </div>
   </div>
 </template>
+
+<script>
+import SubmitForm from "../components/SubmitForm.vue"
+/* 変更点１ */
+import { collection, addDoc, getDocs } from "firebase/firestore"
+import { db } from "../firebase"
+
+export default {
+  components: { SubmitForm },
+
+  data() {
+    return {
+      /* 変更点２ */
+      tweets: [
+        // {
+        //   id: "0GwoGZuhTNhqHQDBeiVW",
+        //   text: "こんにちは、ツイートの本文です。"
+        // }
+      ],
+    }
+  },
+  methods: {
+    postTweet() {
+      const tweet = {
+        text: "旅行の詳細を投稿しました。",
+
+        money: 30000,
+
+        place: "北海道",
+
+        people: 5,
+      }
+      addDoc(collection(db, "tweets"), tweet).then((ref) => {
+        this.tweets.push({
+          id: ref.id,
+          ...tweet,
+        })
+      })
+    },
+  },
+
+  /* 変更点３ */
+  created() {
+    getDocs(collection(db, "tweets")).then((snapshot) => {
+      snapshot.forEach((doc) => {
+        this.tweets.push({
+          id: doc.id,
+          ...doc.data(),
+        })
+      })
+    })
+  },
+}
+</script>
+
+<style scoped>
+.home__wrapper {
+  margin: 0 auto;
+  max-width: 600px;
+  background-color: #ccc;
+}
+</style>
