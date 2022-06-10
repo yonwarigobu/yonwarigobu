e318672405390cdb5952c042d8846633f0cca26b
KentaroDobashi <popkentaro3232@gmail.com>
2022-06-10 18:29:37 +0900

    ホームのHelloWorldを削除しました。

--- src/views/HomeApp.vue
+++ src/views/HomeApp.vue
@@ -1,20 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
     <Logout />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue"
 import Logout from "@/components/Logout.vue"
 
 export default {
   name: "HomeApp",
   components: {
-    HelloWorld,
     Logout,
   },
   data() {
