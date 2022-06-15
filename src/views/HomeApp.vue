a645547b824f884e435139be800f7db0d3476f31
yuka120 <yuka.cherry.0926@icloud.com>
2022-06-15 18:43:55 +0900

    いったん保存

--- src/views/HomeApp.vue
+++ src/views/HomeApp.vue
@@ -2,19 +2,32 @@
   <div class="etsuran">
     <div class="etsuranbox" v-for="tweet in tweetArray" :key="tweet">
       <div class="infoContainer1">
-        <div class="info">場所：</div>
-        <span class="inputInfo">{{ tweet.place }}</span>
-        <div class="info">人数：</div>
-        <span class="inputInfo">{{ tweet.people }}</span
-        >>
-        <div class="info">出費：{{ tweet.moneyS }}</div>
-        <div class="info">目的：{{ tweet.purpose }}</div>
-        <div class="info">期間：{{ tweet.period }}</div>
-        <div class="info">季節：{{ tweet.season }}</div>
+        <div class="info">
+          場所：<span class="inputInfo">{{ tweet.place }}</span>
+        </div>
+        <div class="info">
+          人数： <span class="inputInfo">{{ tweet.people }}</span>
+        </div>
+        <div class="info">
+          出費：<span class="inputInfo">{{ tweet.money }}</span>
+        </div>
+        <div class="info">
+          目的：<span class="inputInfo">{{ tweet.purpose }}</span>
+        </div>
+        <div class="info">
+          仲間：<span class="inputInfo">{{ tweet.relationships }}</span>
+        </div>
+        <div class="info">
+          期間：<span class="inputInfo">{{ tweet.kikan }}</span>
+        </div>
+        <div class="info">
+          季節：<span class="kansouInfo">{{ tweet.season }}</span>
+        </div>
       </div>
       <div class="infoContainer2">
         <img class="info" v-if="img_url" :src="img_url" />
-        <div class="info">感想{{ tweet.text }}</div>
+        <div class="info">感想:</div>
+        <div class="inputInfo">{{ tweet.kansou }}</div>
       </div>
     </div>
   </div>
@@ -60,5 +73,18 @@ export default {
   margin: 20px;
   text-align: left;
   font-weight: 600;
+}
+.inputInfo {
+  top: 10px;
+  left: 200px;
+  margin: 20px;
+  text-align: left;
+  font-weight: 200;
+}
+.kansouInfo {
+  left: 200px;
+  margin-bottom: 20px;
+  text-align: left;
+  font-weight: 200;
 }
 </style>
