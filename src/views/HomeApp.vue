99295dfcff27402a987f227c2a6720080b69f66f
yuka120 <yuka.cherry.0926@icloud.com>
2022-06-18 16:41:52 +0900

    エラーあり

--- src/views/HomeApp.vue
+++ src/views/HomeApp.vue
@@ -28,10 +28,7 @@
         </div>
       </div>
       <div class="infoContainer2">
-        <img class="info" v-if="img_url" :src="img_url" />
-        <div v-if="url">
-          <img :src="url" />
-        </div>
+        <img class="info" v-if="img_url" :src="img_url" />s
         <div class="info">感想:</div>
         <div class="inputInfo">{{ tweet.kansou }}</div>
       </div>
--- src/views/PostApp.vue
+++ src/views/PostApp.vue
@@ -56,6 +56,12 @@
 </template>
 
 <script>
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "firebase/storage"
 import { collection, addDoc } from "firebase/firestore"
 import { db } from "../firebase"
 import HeaderApp from "@/components/HeaderApp.vue"
@@ -98,6 +104,40 @@ export default {
 
         kikan: this.kikan,
       }
+      const file = this.$refs.postImage.files[0]
+      const storage = getStorage()
+      const storageRef = ref(storage, file)
+      const uploadTask = uploadBytesResumable(storageRef, file)
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          // Observe state change events such as progress, pause, and resume
+          // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+          console.log("Upload is " + progress + "% done")
+          switch (snapshot.state) {
+            case "paused":
+              console.log("Upload is paused")
+              break
+            case "running":
+              console.log("Upload is running")
+              break
+          }
+        },
+        (error) => {
+          console.log(error)
+          // Handle unsuccessful uploads
+        },
+        () => {
+          // Handle successful uploads on complete
+          // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            console.log("File available at", downloadURL)
+          })
+        }
+      )
+
       addDoc(collection(db, "tweets"), tweet).then((ref) => {
         this.tweets.push({
           id: ref.id,
